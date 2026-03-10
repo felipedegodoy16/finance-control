8fcdb49e6c10db60ab2c4955282771d3aed47d81
Felipe Godoy <felipedegodoy.dev@gmail.com>
2026-03-09 21:14:03 -0300

    Fix: Add missing oauth2 dependency and simplify static storage to fix 500 error

--- backend/finance_api/settings.py
+++ backend/finance_api/settings.py
@@ -126,4 +126,4 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
